3eeda23be28feb939b07dd86da898c77a3c42098
ruari8 <100523935+ruari8@users.noreply.github.com>
2026-04-21 18:40:46 +0100

    Add: largest-rectangle-in-histogram - submission-4

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-4.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    largestRectangleArea(heights) {
+        // this was fucking impossible for me to understand
+
+        let largestArea = 0
+        let stack = []
+        for(let i=0; i<heights.length; i++) {
+            while(stack.length > 0 && heights[i] < heights[stack[stack.length-1]]) {
+                let pop = stack.pop()
+                let rightSmallerIndex = i
+                let leftSmallerIndex = stack.length ? stack[stack.length - 1] : -1
+                let width = rightSmallerIndex-leftSmallerIndex-1
+                let height = heights[pop]
+                let area = width*height
+                if(area > largestArea) largestArea = area
+            }
+            stack.push(i)
+        }
+        while(stack.length > 0) {
+            let pop = stack.pop()
+            let rightSmallerIndex = heights.length
+            let leftSmallerIndex = stack.length ? stack[stack.length - 1] : -1
+            let width = rightSmallerIndex-leftSmallerIndex-1
+            let height = heights[pop]
+            let area = width*height
+            if(area > largestArea) largestArea = area
+        }
+        return largestArea
+    }
+}
